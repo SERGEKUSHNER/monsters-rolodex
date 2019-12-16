37f0c22ce89c32d509322f976d815b6c41d440c7
Sergiu <kusnir.sergiu@gmail.com>
2019-12-16 14:40:28 +0200

    Updates

---
+++ precache-manifest.0c6f5bdece3ea4cb702d9f9a82cae53f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00c703eb87cbae10d4d63658a4ed0b00",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "1658d4b9ba55129dc639",
+    "url": "/monsters-rolodex/static/css/main.e3a68701.chunk.css"
+  },
+  {
+    "revision": "f5c59ff7d2bcec7481a7",
+    "url": "/monsters-rolodex/static/js/2.040d0fe2.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.040d0fe2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1658d4b9ba55129dc639",
+    "url": "/monsters-rolodex/static/js/main.66a45653.chunk.js"
+  },
+  {
+    "revision": "dc25ba12107eb546721c",
+    "url": "/monsters-rolodex/static/js/runtime-main.75c8e44f.js"
+  }
+]);
